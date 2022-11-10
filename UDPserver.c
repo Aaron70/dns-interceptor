22d97fc9a549ce4a114a042de87f82553fd6f608
Aaron <aaron@Aaron.Aaron>
2022-11-10 12:31:50 -0600

    Validating if a dns query is standart

--- UDPserver.c
+++ UDPserver.c
@@ -11,10 +11,24 @@
 #define PORT	 53
 #define MAXLINE 1024
 	
+char* intToIpv4( int ip){
+	char* ipv4[4];
+	int byte[4];
+	for( int i = 0; i < 4; i++ ){
+		byte[i] = ip & 255;
+		ip = ip >> 8;
+	}
+	char* ipAddress = malloc(15);
+	sprintf(ipAddress, "%i.%i.%i.%i", byte[3], byte[2], byte[1], byte[0]);
+	return ipAddress;
+}
+
+
+
 // Driver code
 int main() {
 	int sockfd;
-	char buffer[MAXLINE];
+	int buffer[MAXLINE];
 	char *hello = "Hello from server";
 	struct sockaddr_in servaddr, cliaddr;
 		
@@ -44,12 +58,22 @@ int main() {
 	
 	len = sizeof(cliaddr); //len is value/result
 	
-	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+	n = recvfrom(sockfd, (int *)buffer, MAXLINE,
 				MSG_WAITALL, ( struct sockaddr *) &cliaddr,
 				&len);
-	buffer[n] = '\0';
+	//buffer[n] = '\0';
+	struct DNS_HEADER *dns = (struct DNS_HEADER*)&buffer; 
+	unsigned short int id = dns->id;
+	printf("Number of bytes readed: %d/n",n);
+	printf("ID: %s\n", intToIpv4(dns->id));
+	printf("ID: %hu\n", dns->id);
+	printf("ID: %hu\n", (id >> 8 ) + ((id & 255) << 8));
+	printf("Qr: %i\n", 0+dns->qr);
+	printf("Opcode: %i\n", 0+dns->opcode);
+	printf("Query Count: %i\n", 0+dns->q_count);
 	printf("Client : %s\n", buffer);
-	sendto(sockfd, (const char *)hello, strlen(hello),
+	dns->qr = 1;
+	sendto(sockfd, (const char *)dns, sizeof(buffer),
 		MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
 			len);
 	printf("Hello message sent.\n");
