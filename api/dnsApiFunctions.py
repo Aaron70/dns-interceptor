784899a5c1d362429ff0c1ed92c6691f80868c0d
DESKTOP-4J2GL01\in_ar <ingrid09arce@gmail.com>
2022-11-14 20:02:08 -0600

    coding and decoding base 64 api

---
+++ api/dnsApiFunctions.py
@@ -0,0 +1,35 @@
+import base64
+from dnslib import DNSRecord
+
+def base64Encoder(data):
+    data_bytes = data.encode('utf-8')
+    data_b64 = base64.b64encode(data_bytes)
+    data_b64_string = data_b64.decode('utf-8')
+    return data_b64_string
+
+def base64Decoder(data):
+    data_b64 = data.encode('utf-8')
+    data_bytes = base64.b64decode(data_b64)
+    data_string = data_bytes.decode('utf-8')
+    return data_string
+
+
+def socketUdp(query):
+    #msgFromClient       = fun2()
+    bytesToSend         = str.encode(query)
+    serverAddressPort   = ("127.0.0.1", 53)
+    bufferSize          = 1024
+
+    # Create a UDP socket at client side
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    # Send to server using created UDP socket
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    #client2(msgFromServer)
+    msg = "Message from Server {}".format(msgFromServer[0])
+    d = DNSRecord.parse(msgFromServer[0])
+
+    #c = BitArray(hex= msgFromServer[0])
+    #print(c.bin)
+    print(d)
+    return d
